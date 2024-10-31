43840bae038070626ed715b9e938e0fcf9b50984
ljs <artbiit@naver.com>
2024-10-31 20:30:39 +0900

    불필요한 메세지 제거

--- src/utils/notification/game.notification.js
+++ src/utils/notification/game.notification.js
@@ -1,19 +1,8 @@
 import { handlerIds } from '../../handlers/index.js';
-import { getProtoMessages } from '../../init/loadProtos.js';
 import { createResponse } from '../response/createResponse.js';
 
 export const createLocationPacket = (users) => {
   return createResponse(handlerIds['gameNotification.LocationUpdate'], 0, { users }, null);
-};
-
-export const gameStartNotification = (gameId, timestamp) => {
-  const protoMessages = getProtoMessages();
-  const Start = protoMessages.gameNotification.Start;
-
-  const payload = { gameId, timestamp };
-  const message = Start.create(payload);
-  const startPacket = Start.encode(message).finish();
-  //return makeNotification(startPacket, PACKET_TYPE.GAME_START);
 };
 
 export const createPingPacket = (timestamp) => {
